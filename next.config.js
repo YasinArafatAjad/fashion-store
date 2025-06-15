f5f8ab2a337a7671bd8121df3e2a8b29f068d285
Yasin Arafat Ajad <136703649+YasinArafatAjad@users.noreply.github.com>
2025-06-15 23:59:18 +0600

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { 
     unoptimized: true,
     domains: ['res.cloudinary.com', 'images.unsplash.com']
-  }
+  },
+  // Remove output: 'export' for development
+  // output: 'export',
+  // trailingSlash: true,
 };
 
 module.exports = nextConfig;
